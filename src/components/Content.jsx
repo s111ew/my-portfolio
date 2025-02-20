33a1833f0bdc8006bb2de1ecf5712957f87ae8ec
s111ew <167631312+s111ew@users.noreply.github.com>
2025-02-20 18:48:00 +0000

    Add custom positioning on load for folder icons

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,19 +1,27 @@
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import folderImg from "../assets/folder.webp";
+const PADDING = 25;
 
 function Content() {
   return (
     <main>
-      <Folder text={"Work"} />
-      <Folder text={"Words"} />
+      <Folder side={"left"} text={"Work"} />
+      <Folder side={"right"} text={"Words"} />
     </main>
   );
 }
 
-function Folder({ text }) {
+function Folder({ side, text }) {
   const folderRef = useRef(null);
   const startX = useRef(0);
   const startY = useRef(0);
+  const [position, setPosition] = useState({ top: "50px", left: "50px" });
+
+  useEffect(() => {
+    if (folderRef.current) {
+      setPosition(generateLocation(side));
+    }
+  }, [side]);
 
   const handleMouseDown = (e) => {
     startX.current = e.clientX;
@@ -28,20 +36,32 @@ function Folder({ text }) {
 
     folderRef.current.style.cursor = "grab";
 
-    const containerSize = folderRef.current.parentElement;
-    console.log(containerSize)
-    //Work out container size and if new location is outside of this return??
+    const containerWidth = folderRef.current.parentElement.getBoundingClientRect().width;
+    const containerHeight = folderRef.current.parentElement.getBoundingClientRect().height;
+
+    const folderWidth = folderRef.current.offsetWidth;
+    const folderHeight = folderRef.current.offsetHeight;
+
+    const boundingLeft = PADDING;
+    const boundingRight = containerWidth - PADDING - folderWidth;
+    const boundingTop = PADDING;
+    const boundingBottom = containerHeight - PADDING - folderHeight;
 
     let newX = startX.current - e.clientX;
     let newY = startY.current - e.clientY;
 
+    let newTop = folderRef.current.offsetTop - newY;
+    let newLeft = folderRef.current.offsetLeft - newX;
+
+    if (newTop > boundingBottom || newTop < boundingTop || newLeft > boundingRight || newLeft < boundingLeft) {
+        return
+      }
+
+    folderRef.current.style.top = newTop + "px"
+    folderRef.current.style.left = newLeft + "px"
+
     startX.current = e.clientX;
     startY.current = e.clientY;
-
-    folderRef.current.style.top = folderRef.current.offsetTop - newY + "px";
-    folderRef.current.style.left = folderRef.current.offsetLeft - newX + "px";
-
-    console.log(folderRef.current.style.top, folderRef.current.style.left)
   };
 
   const handleMouseUp = () => {
@@ -50,8 +70,31 @@ function Folder({ text }) {
     document.removeEventListener("mouseup", handleMouseUp);
   };
 
+  const generateLocation = (side) => {
+    //REFACTOR THIS TO GENERATE A ROUGH LOCATION IN THE SPOT YOU WANT, BASED OFF OF THE WINDOW SIZE.
+    //THEN CREATE FUNCTION TO LISTEN FOR WINDOW RESIZE (new style.top = old style.top +- (new windowsize - old windowsize)  )
+    if (!folderRef.current) return side === "left" ? { top: "50px", left: "50px" } : { top: "170px", left: "190px" }
+
+    const minTop = PADDING * 2;
+    const maxTop = folderRef.current.parentElement.getBoundingClientRect().height - PADDING * 2;
+    const minLeft = side === "left" ? PADDING * 2 : Math.ceil(folderRef.current.parentElement.getBoundingClientRect().width / 2) - PADDING;
+    const maxLeft = side === "left" ? Math.floor(folderRef.current.parentElement.getBoundingClientRect().width / 2) - PADDING : folderRef.current.parentElement.getBoundingClientRect().width - PADDING * 2;
+
+    const getRandomValue = (min, max) => {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
+    return {
+      top: getRandomValue(minTop, maxTop) + "px",
+      left: getRandomValue(minLeft, maxLeft) + "px"
+    }
+  }
+
   return (
     <div
+      style={{ top: position.top, left: position.left }}
       ref={folderRef}
       className="folder-container"
       onMouseDown={handleMouseDown}
