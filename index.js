a5e8ea6d3c996ade2d1060770b16f07153301560
s111ew <167631312+s111ew@users.noreply.github.com>
2025-07-15 23:17:30 +0100

    add keyboard navigation to projects

--- index.js
+++ index.js
@@ -4,11 +4,38 @@ function onLoad() {
 
 function addListeners() {
   const projects = document.querySelectorAll(".project");
+  const labels = document.querySelectorAll(".index");
 
   projects.forEach((project) => {
     project.addEventListener("click", (e) => {
       if (e.target.closest(".button")) return;
       project.classList.toggle("active");
+
+      const buttons = project.querySelectorAll(".button");
+
+      if (project.classList.contains("active")) {
+        buttons.forEach((btn) => (btn.tabIndex = 0));
+      } else {
+        buttons.forEach((btn) => (btn.tabIndex = -1));
+      }
+    });
+  });
+
+  labels.forEach((label) => {
+    label.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        const project = label.parentElement;
+        if (project) {
+          project.classList.toggle("active");
+          const buttons = project.querySelectorAll(".button");
+
+          if (project.classList.contains("active")) {
+            buttons.forEach((btn) => (btn.tabIndex = 0));
+          } else {
+            buttons.forEach((btn) => (btn.tabIndex = -1));
+          }
+        }
+      }
     });
   });
 }
